7aeaa9edde2b4850d746f38b0d7547bd5971d319
samimhm <mhm.sami@yahoo.com>
2023-04-11 02:34:59 +0300

    adding product card and cart

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,24 @@
+import './cart-dropdown.styles.scss'
+import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
+const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext)
+
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                {
+                    cartItems.map(item => {
+                        return <CartItem cartItem={item} key={item.id}/>
+                    })
+                }
+            </div>
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    )
+}
+
+export default CartDropdown
--- src/index.js
+++ src/index.js
@@ -8,13 +8,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
